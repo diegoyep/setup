27932bf86283726551acbff792caf16807dda479
Ubuntu <ubuntu@ip-10-200-143-210.us-west-1.compute.internal>
2014-02-25 15:48:24 +0000

    first setup

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+apt-get update
+apt-get dist-upgrade
+apt-get -y install apache2
+apt-get -y install php5 libapache2-mod-php5
+/etc/init.d/apache2 restart
+
+sudo adduser ubuntu www-data
+sudo chown -R www-data:www-data /var/www
+sudo chmod -R g+rw /var/www
+
+echo mysql-server-5.1 mysql-server/root_password password finalf  | debconf-set-selections
+echo mysql-server-5.1 mysql-server/root_password_again password finalf | debconf-set-selections
+apt-get install -y mysql-server
+
+sudo apt-get install phpmyadmin
+sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf
+
+sudo /etc/init.d/apache2 reload
+
+
+exit 0
+
