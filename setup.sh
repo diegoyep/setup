c26bba9252abfd274945e83952ad07786d9adecc
Diego <diego.yepes@zefira.pe>
2014-02-27 18:41:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apt-get update
-apt-get dist-upgrade
+apt-get -y update
+apt-get -y dist-upgrade
 apt-get -y install apache2
 apt-get -y install php5 libapache2-mod-php5
 /etc/init.d/apache2 restart
@@ -19,7 +19,7 @@ sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf
 
 sudo /etc/init.d/apache2 reload
 
-sudo apt-get install unzip
+sudo apt-get install -y unzip
 wget http://ellislab.com/asset/ci_download_files/reactor/CodeIgniter_2.1.3.zip
 unzip CodeIgniter_2.1.3.zip
 
