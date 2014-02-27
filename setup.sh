c84a03282e53ebfd2a0b2d52c463327cc10859dc
Diego <diego.yepes@zefira.pe>
2014-02-27 18:32:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,10 +36,24 @@ sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 wget https://raw.githubusercontent.com/ejmr/php-mode/master/php-mode.el
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/startup-class/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
 mv php-mode.el ~/.emacs.d
 
 echo "change root file on /etc/apache2/site-availables/default file"
-echo "and add php-mode to emacs
+echo "and add php-mode to emacs"
 
 exit 0
 
